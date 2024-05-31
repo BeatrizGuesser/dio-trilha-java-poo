3d7bbdbb67173a9410a73cff32d66431480fb06a
Beatriz <biabagg12@gmail.com>
2024-05-31 20:21:17 -0300

    test methods

--- iphone/src/App.java
+++ iphone/src/App.java
@@ -1,5 +1,22 @@
+import model.Iphone;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Iphone iphone = new Iphone();
+
+        // Testando ReprodutorMusical
+        iphone.selecionarMusica("Too Sweet - Hozier");
+        iphone.tocar();
+        iphone.pausar();
+
+        // Testando AparelhoTelefonico
+        iphone.ligar("123456789");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        // Testando NavegadorInternet
+        iphone.exibirPagina("www.linkedin.com/in/beatriz-gorges-guesser/");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
     }
 }
